8800fa9b082f6b3674f6efb1dda8d18e86bf2899
root <ac0x3f@163.com>
2024-06-14 20:09:02 +0800

    新增generator模块，代码生成器

---
+++ generator/src/main/java/com/hasity/App.java
@@ -0,0 +1,13 @@
+package com.hasity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
